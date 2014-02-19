10d778f049bd8e1993901c90ee81306ad9d7d5ba
Nguyễn Tuấn Anh <anhnt.fami@gmail.com>
2014-02-19 17:21:11 +0700

    DB

--- app/controllers/db.js
+++ app/controllers/db.js
@@ -9,24 +9,35 @@ var mongoose = require('mongoose'),
  		return mongoose.models[name];
  	};
  	DB.find = function(){
- 		var deferred = Q.defer();
- 		var name = arguments[0];
- 		var args = [];
+ 		var deferred = Q.defer(),
+ 			name = arguments[0],
+ 			args = [];
+ 		
+ 		//Change arguments into an array
  		for (var i = 1, l = arguments.length; i < l; i++) {
  			args.push(arguments[i]);
  		};
- 		var model = mongoose.models[name];
- 		var model_path = model.schema.paths;
+
+ 		//Caching path
+ 		var model = mongoose.models[name],
+ 			model_path = model.schema.paths;
+
+ 		//Find
  		var result = model.find.apply(model,args);
+
+ 		//Looks for ObjectID fields & populate them
 		for (prop in model_path){
 			if (model_path[prop].caster 
 				&& model_path[prop].caster.instance==='ObjectID')
+				//Populate data
 				result = result.populate(prop);
 		}
+
 		result.exec(function(err, found){
 			if (err) deferred.reject();
 	    	deferred.resolve(found);
 	    });
+	    
 	    return deferred.promise;
  	};
  	app.createModule('DB', DB);
