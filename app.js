d524e9dc8f0881994035a26ca8a2d7bd43dd521a
Nguyễn Tuấn Anh <anhnt.fami@gmail.com>
2014-02-19 14:40:07 +0700

    Update personal framework

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require('express'),
   fs = require('fs'),
   config = require('./config/config'),
   facebook = require('./config/facebook');
+  readDir = require('./lib/readDir'),
+  Q = require('q');
 
 mongoose.connect(config.db);
 var db = mongoose.connection;
@@ -13,14 +15,56 @@ db.on('error', function () {
 });
 
 var modelsPath = __dirname + '/app/models';
-fs.readdirSync(modelsPath).forEach(function (file) {
-  if (file.indexOf('.js') >= 0) {
-    require(modelsPath + '/' + file);
-  }
+readDir(modelsPath, function(err, data){
+	data.files.forEach(function(file) {
+		if (file.length - file.indexOf('.js') === 3) {
+			require(file);
+		}
+	});
 });
 
 var app = express();
 app.httpServer = app.listen(config.port);
 
+var modules = {},
+	modulesDeferreds = {};
+
+app.createModule = function(name, module, override){
+	if (modules[name] && !override){
+		console.error('Module '+name+' has already been defined');
+		process.exit(1);
+	} else {
+		modules[name] = module;
+		if (modulesDeferreds[name]){
+			while (modulesDeferreds[name].length > 0){
+				modulesDeferreds[name].pop().resolve(module);
+			}
+			delete modulesDeferreds[name];
+		}
+	}
+};
+
+app.require = function(listOfModules, fn){
+	var listOfPromise = [];
+	for (var i = 0, l = listOfModules.length; i < l; i++){
+		var name = listOfModules[i],
+			deferred = Q.defer();
+		if (modules[name]) deferred.resolve(modules[name]);
+		else {
+			modulesDeferreds[name] = modulesDeferreds[name] || [];
+			modulesDeferreds[name].push(deferred);
+		};
+		listOfPromise.push(deferred.promise);
+	}
+	Q.all(listOfPromise).spread(fn);
+};
 require('./config/express')(app, config);
-require('./config/routes')(app);
+require('./config/routes')(app)
+.then(function(){
+	if (Object.keys(modulesDeferreds).length > 0){
+		for (name in modulesDeferreds){
+			console.error('Module '+name+' not defined');
+		}
+		process.exit(1);
+	}
+});
--- config/routes.js
+++ config/routes.js
@@ -3,56 +3,25 @@ var express = require('express'),
 	Q = require('q'),
 	readDir = require('../lib/readDir');
 
+var express = require('express'),
+	fs = require('fs'),
+	Q = require('q'),
+	readDir = require('../lib/readDir');
+
 module.exports = function(app){
-
-	var modules = {},
-		modulesDeferreds = {};
-
-	app.createModule = function(name, module, override){
-		if (modules[name] && !override){
-			console.error('Module '+name+' has already been defined');
-			process.exit(1);
-		} else {
-			modules[name] = module;
-			if (modulesDeferreds[name]){
-				while (modulesDeferreds[name].length > 0){
-					modulesDeferreds[name].pop().resolve(module);
-				}
-				delete modulesDeferreds[name];
-			}
-		}
-	};
-
-	app.require = function(listOfModules, fn){
-		var listOfPromise = [];
-		for (var i = 0, l = listOfModules.length; i < l; i++){
-			var name = listOfModules[i],
-				deferred = Q.defer();
-			if (modules[name]) deferred.resolve(modules[name]);
-			else {
-				modulesDeferreds[name] = modulesDeferreds[name] || [];
-				modulesDeferreds[name].push(deferred);
-			};
-			listOfPromise.push(deferred.promise);
-		}
-		Q.all(listOfPromise).spread(fn);
-	};
-
+	var deferred = Q.defer();
 	var modelsPath = __dirname + '/../app/controllers';
 	fs.readdirSync(modelsPath)
 
 	readDir(modelsPath, function(err, data){
-		data.files.forEach(function(file) {
+		if (err) deferred.reject();
+		else data.files.forEach(function(file) {
 			if (file.length - file.indexOf('.js') === 3) {
 				require(file)(app);
 			}
+			deferred.resolve();
 		});
-		if (Object.keys(modulesDeferreds).length > 0){
-			for (name in modulesDeferreds){
-				console.error('Module '+name+' not defined');
-			}
-			process.exit(1);
-		}
-	})
+	});
 
+	return deferred.promise;
 };
